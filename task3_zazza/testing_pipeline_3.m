b5701c9d0fdd6816e3b3872d9216eb24ba8590f8
Elisa Pace <pace.elisa1@gmail.com>
2025-03-17 02:17:36 +0100

    Update task3_zazza\testing_pipeline_3.m, add some changes to output lines.

--- task3_zazza/testing_pipeline_3.m
+++ task3_zazza/testing_pipeline_3.m
@@ -1,6 +1,7 @@
 % Loading of the test dataset composed of bubble contamination anomalies, obtained by filtering
 % the results obtained by task 2
-load("./preprocessingData/filteredTestSet.mat", "filteredTestSet");  
+load("./preprocessingData/filteredTestSet.mat", "filteredTestSet"); 
+disp( [newline '__Task3__']);
 disp(['Number of samples in the test set: ', num2str(height(filteredTestSet))]);
 
 % loading of the table with predictions
@@ -82,7 +83,7 @@ disp(['Model Accuracy: ', num2str(accuracy), '%']);
 % Confusion Matrix
 
 classLabels = {'Other', 'BP1', 'BP2', 'BP3', 'BP4', 'BP5', 'BP6', 'BP7'};
-C = confusionmat(trueLabels, predictedLabels)
+C = confusionmat(trueLabels, predictedLabels);
 figure;
 subplot(1,2,1);
 confusionchart(C, classLabels);
